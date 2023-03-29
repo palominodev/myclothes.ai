4aaf4ee5733d96fdf7d422c890117f36b6288451
palominodev <jeremypalomino.dev@gmail.com>
2023-03-29 21:36:22 +0200

    pruebas: camara

--- src/pages/QRPage.jsx
+++ src/pages/QRPage.jsx
@@ -1,7 +1,30 @@
 import React from 'react'
+import Webcam from 'react-webcam'
+
+const videoConstraints = {
+  width: 1280,
+  height: 720,
+  facingMode: "user"
+};
 
 export const QRPage = () => {
   return (
-    <h1>QRPage</h1>
+    <Webcam
+      audio={false}
+      height={720}
+      screenshotFormat="image/jpeg"
+      width={1280}
+      videoConstraints={videoConstraints}
+  >
+    {({ getScreenshot }) => (
+      <button
+        onClick={() => {
+          const imageSrc = getScreenshot()
+        }}
+      >
+        Capture photo
+      </button>
+    )}
+  </Webcam>
   )
 }
